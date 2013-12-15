66391d517d08b03323e80b945c424d205ab21a65
34744 <cedric_deceuleneer@hotmail.com>
2013-12-15 11:23:39 +0100

    Connexion DB OK

---
+++ src/controller/Main.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		view.Connection connect = new view.Connection();
+		
+	}
+
+}
